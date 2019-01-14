f091d91f876c7990f63debea8a3bdc07f97bf2cb
unknown <pprabh2007@hotmail.co.uk>
2019-01-14 13:45:55 +0530

    Add Excel Support

--- ReadSer.java
+++ ReadSer.java
@@ -1,6 +1,12 @@
 import java.io.*;
 import java.util.*; 
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook; 
+import org.apache.poi.ss.usermodel.Cell; 
+import org.apache.poi.ss.usermodel.Row; 
+import org.apache.poi.ss.usermodel.Sheet; 
+import org.apache.poi.ss.usermodel.Workbook; 
+
 public class ReadSer {
 	
 	public static void main(String args[])
@@ -32,24 +38,44 @@ public class ReadSer {
 			
 		}
 		
-		int number_of_tweets=tweet_list.size();
-		
-		for(int i=0; i<number_of_tweets; i++)
+		try
 		{
-			tweet_list.get(i).printTweet();
-		}
-		
-		for(int i=0; i<number_of_tweets; i++)
-		{
-			for(int j=i+1; j<number_of_tweets; j++)
+			Workbook workbook = new HSSFWorkbook();
+			OutputStream writer = new FileOutputStream("Datasheet.xls");
+			Sheet sheet = workbook.createSheet("Features");
+			
+			int number_of_tweets=tweet_list.size();
+			Row[] rows=new Row[number_of_tweets];
+			
+			for(int i=0; i<number_of_tweets; i++)
 			{
-				TextProcessing tps=new TextProcessing(tweet_list.get(i), tweet_list.get(j));
-				String features_of_pair=(tps.getFeatures());
-
+				rows[i] = sheet.createRow(i);
+				Cell itself=rows[i].createCell(i);
 			}
+			
+			for(int i=0; i<number_of_tweets; i++)
+			{
+				for(int j=i; j<number_of_tweets; j++)
+				{
+					TextProcessing tps=new TextProcessing(tweet_list.get(i), tweet_list.get(j));
+					String features_of_pair=(tps.getFeatures());
+					
+					 Cell cell_one = rows[i].createCell(j);
+					 cell_one.setCellValue(features_of_pair); 
+					 	
+					 Cell cell_two = rows[j].createCell(i);
+					 cell_two.setCellValue(features_of_pair); 
+					 System.out.println(i*number_of_tweets+j);
+				}
+			}
+			workbook.write(writer);
+			writer.close();
+			workbook.close();
 		}
-		
-		
+		catch(Exception  e)
+		{
+			e.printStackTrace();
+		}
 		
 	}
 
