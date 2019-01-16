e939999abe6506d72bcf49e932469ad26285d149
unknown <pprabh2007@hotmail.co.uk>
2019-01-16 16:44:01 +0530

    Add bat files to run on window, plus taggers and sample data

---
+++ compile.bat
@@ -0,0 +1,4 @@
+javac -classpath ".;lib/corenlp/*;lib/ld/*;lib/jsonic/*;lib/poi/*;lib/postagger/*;lib/json/*" StanfordLemmatizer.java
+javac -classpath ".;lib/corenlp/*;lib/ld/*;lib/jsonic/*;lib/poi/*;lib/postagger/*;lib/json/*" ReadJSON.java
+javac -classpath ".;lib/corenlp/*;lib/ld/*;lib/jsonic/*;lib/poi/*;lib/postagger/*;lib/json/*" TextProcessing.java
+javac -classpath ".;lib/corenlp/*;lib/ld/*;lib/jsonic/*;lib/poi/*;lib/postagger/*;lib/json/*" ReadSer.java
---
+++ run.bat
@@ -0,0 +1,2 @@
+java -classpath ".;lib/corenlp/*;lib/ld/*;lib/jsonic/*;lib/poi/*;lib/postagger/*;lib/json/*" ReadJSON
+java -classpath ".;lib/corenlp/*;lib/ld/*;lib/jsonic/*;lib/poi/*;lib/postagger/*;lib/json/*" ReadSer
