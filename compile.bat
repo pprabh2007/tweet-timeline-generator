6c4d8226ed5ed7b2fb96774d39693509fc252e1d
unknown <pprabh2007@hotmail.co.uk>
2019-01-17 15:28:32 +0530

    Version for Ubuntu

--- compile.bat
+++
@@ -1,4 +0,0 @@
-javac -classpath ".;lib/corenlp/*;lib/ld/*;lib/jsonic/*;lib/poi/*;lib/postagger/*;lib/json/*" StanfordLemmatizer.java
-javac -classpath ".;lib/corenlp/*;lib/ld/*;lib/jsonic/*;lib/poi/*;lib/postagger/*;lib/json/*" ReadJSON.java
-javac -classpath ".;lib/corenlp/*;lib/ld/*;lib/jsonic/*;lib/poi/*;lib/postagger/*;lib/json/*" TextProcessing.java
-javac -classpath ".;lib/corenlp/*;lib/ld/*;lib/jsonic/*;lib/poi/*;lib/postagger/*;lib/json/*" ReadSer.java
--- run.bat
+++
@@ -1,2 +0,0 @@
-java -classpath ".;lib/corenlp/*;lib/ld/*;lib/jsonic/*;lib/poi/*;lib/postagger/*;lib/json/*" ReadJSON
-java -classpath ".;lib/corenlp/*;lib/ld/*;lib/jsonic/*;lib/poi/*;lib/postagger/*;lib/json/*" ReadSer
